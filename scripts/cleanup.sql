f95e601f834ad8c1095e8323ba6ebef8f08b2809
Randal Hale <rjhale@northrivergeographic.com>
2021-11-02 16:17:26 -0400

    fixed squence for editing

--- scripts/cleanup.sql
+++ scripts/cleanup.sql
@@ -16,4 +16,13 @@ alter table tn911.address_points add primary key (id);
 
 update tn911.centerlines set segid = id;
 
+create sequence tn911.address_points_idpk_seq; 
+alter sequence tn911.address_points_idpk_seq owned by tn911.centerlines.id; 
+SELECT setval(pg_get_serial_sequence('tn911.address_points', 'id'), coalesce(max(id),0) + 1, false) FROM tn911.address_points;
+alter table tn911.address_points alter column id set default nextval('tn911.address_idpk_seq');
 
+
+create sequence tn911.centerlines_idpk_seq; 
+alter sequence tn911.centerlines_idpk_seq owned by tn911.centerlines.id; 
+SELECT setval(pg_get_serial_sequence('tn911.centerlines', 'id'), coalesce(max(id),0) + 1, false) FROM tn911.centerlines;
+alter table tn911.centerlines alter column id set default nextval('tn911.centerlines_idpk_seq');
