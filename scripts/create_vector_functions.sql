7e43e00f96e2276935261058202f73b33eda34f7
Randal Hale <rjhale@northrivergeographic.com>
2021-11-03 06:59:16 -0400

    sep out dem and vector functions

--- scripts/create_vector_functions.sql
+++ scripts/create_vector_functions.sql
@@ -106,20 +106,6 @@ CREATE TRIGGER update_address_location BEFORE insert or update
     ON tn911.address_points FOR EACH ROW EXECUTE PROCEDURE
     address_location_func();
 
-/*address elevation/ 
-
-CREATE OR REPLACE FUNCTION address_elev_func()
-RETURNS TRIGGER AS '
-BEGIN
-  NEW.z_val := (select round(st_value(rast, NEW.geom)/3.28081) from dem.henry_2017 WHERE ST_Intersects(rast, NEW.geom)=true);
-  RETURN NEW; 
-END; 
-' language 'plpgsql';
-
-CREATE TRIGGER update_address_elev BEFORE insert or update
-    ON tn911.address_points FOR EACH ROW EXECUTE PROCEDURE
-    address_elev_func();
-
 
 /*address oirid*/ 
 
