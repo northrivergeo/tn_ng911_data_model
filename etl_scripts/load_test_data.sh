8e8bab2fd21bae90134b4c1bd4ac8bd3f80f421c
Kyle <ksnyder995@gmail.com>
2022-02-16 18:15:46 -0500

    updated addresspoints to address_points

--- etl_scripts/load_test_data.sh
+++ etl_scripts/load_test_data.sh
@@ -3,6 +3,6 @@
 #change your username and password and make sure your path is correct
 
 ogr2ogr -update -append -f "PostgreSQL" -a_srs "EPSG:2274" PG:"host=localhost user=user dbname=tndemo password=password schemas=tn911" ../data/streets.shp -nln tn911.centerlines -nlt linestring -skipfailures
-ogr2ogr -update -append -f "PostgreSQL" -a_srs "EPSG:2274" PG:"host=localhost user=user dbname=tndemo password=password schemas=tn911" ../data/address_points.shp -nln tn911.addresspoints -nlt point -skipfailures
+ogr2ogr -update -append -f "PostgreSQL" -a_srs "EPSG:2274" PG:"host=localhost user=user dbname=tndemo password=password schemas=tn911" ../data/address_points.shp -nln tn911.address_points -nlt point -skipfailures
 ogr2ogr -update -append -f "PostgreSQL" -a_srs "EPSG:2274" PG:"host=localhost user=user dbname=tndemo password=password schemas=tn911" ../data/esn_area.shp -nln tn911.esn_boundary -skipfailures
 
