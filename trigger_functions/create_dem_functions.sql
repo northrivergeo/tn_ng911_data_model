9039af50ba28c697b06a0783340ae86945e92742
Kyle <ksnyder995@gmail.com>
2022-02-21 11:58:46 -0500

    removed "...vector_functions.sql"

--- trigger_functions/create_dem_functions.sql
+++ trigger_functions/create_dem_functions.sql
@@ -1,12 +1,13 @@
 /*address elevation*/
 
 CREATE OR REPLACE FUNCTION address_elev_func()
-RETURNS TRIGGER AS '
+RETURNS TRIGGER AS $$
 BEGIN
   NEW.z_val := (select round(st_value(rast, NEW.geom)/3.28081) from dem.henry_2017 WHERE ST_Intersects(rast, NEW.geom)=true);
   RETURN NEW; 
 END; 
-' language 'plpgsql';
+$$
+LANGUAGE PLPGSQL;
 
 CREATE TRIGGER update_address_elev BEFORE insert or update
     ON tn911.address_points FOR EACH ROW EXECUTE PROCEDURE
