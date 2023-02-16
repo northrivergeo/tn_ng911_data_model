acb324d1dc21f4560c95dd8ec177b57ee9504455
rjhale1971 <rjhale@northrivergeographic.com>
2023-02-16 13:56:37 -0500

    removing docker

--- docker/docker_install.sh
+++
@@ -1,6 +0,0 @@
-#Hasn't been tested yet. Should setup the kartoza postgis docker image. 
-sudo apt-get install docker.io docker-compose
-sudo apt-get update && sudo apt-get upgrade -y
-sudo docker pull kartoza/postgis:13.0
-sudo docker volume create pg_data
-docker run --name=postgis -d -e POSTGRES_USER=postgres -e POSTGRES_PASS=password -e POSTGRES_DBNAME=ng911 -e ALLOW_IP_RANGE=0.0.0.0/0 -p 5432:5432 -v pg_data:/var/lib/postgresql kartoza/postgis:13.0
