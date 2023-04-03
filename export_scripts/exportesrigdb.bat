f0398a6c16807c133d4934eb87ef447f4d754a20
rjhale1971 <rjhale@gmail.com>
2023-04-03 12:05:07 -0400

    updating export scripts

--- export_scripts/exportesrigdb.bat
+++ export_scripts/exportesrigdb.bat
@@ -1,7 +1,7 @@
 IF EXIST C:\OSGeo4W64\bin SET PATH=%PATH%;C:\OSGeo4W64\bin
 
-ogr2ogr -f OpenFileGDB  TCStransport.gdb -nlt point  PG:"host=192.168.124.234 user=rjhale dbname=county911 password=9wikufi6" "tn911.address_points" -nln address_points 
-ogr2ogr -f OpenFileGDB  TCStransport.gdb -append -nlt linestring  PG:"host=192.168.124.234 user=rjhale dbname=county911 password=9wikufi6" "tn911.centerlines" -nln centerlines 
-ogr2ogr -f OpenFileGDB  TCStransport.gdb -append -nlt polygon  PG:"host=192.168.124.234 user=rjhale dbname=county911 password=9wikufi6" "tn911.esn_boundary" -nln esn_boundary
+ogr2ogr -f OpenFileGDB  TCStransport.gdb -nlt point  PG:"host=host user=user dbname=dbname password=pass" "tn911.address_points" -nln address_points 
+ogr2ogr -f OpenFileGDB  TCStransport.gdb -append -nlt linestring  PG:"host=host user=user dbname=dbname password=pass" "tn911.centerlines" -nln centerlines 
+ogr2ogr -f OpenFileGDB  TCStransport.gdb -append -nlt polygon  PG:"host=host user=user dbname=dbname password=pass" "tn911.esn_boundary" -nln esn_boundary
 
 
