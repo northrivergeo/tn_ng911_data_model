a7d8e949bebc195edc471c31f4f23abfe2aefea7
rjhale <rjhale@gis2.localdomain>
2023-06-12 16:20:11 -0400

    updating qaqc scripts

--- qaqc/left_range.sql
+++ qaqc/left_range.sql
@@ -1,15 +1,15 @@
-select left1.label, left1.l_f_add::int, left1.l_t_add::int, left1.id, left2.label, left2.id, left2.l_f_add::int, left2.l_t_add::int
-from (select id, 
+select left1.label, left1.oirid, left1.r_f_add::int, left1.r_t_add::int, left2.label, left2.oirid, left2.r_f_add::int, left2.r_t_add::int
+from (select oirid, 
              label,
-             l_f_add::int,
-             l_t_add::int 
+             r_f_add::int,
+             r_t_add::int 
       from tn911.centerlines) left1
-inner join (select id, 
+inner join (select oirid, 
              label,
-             l_f_add::int,
-             l_t_add::int 
+             r_f_add::int,
+             r_t_add::int 
       from tn911.centerlines) left2
  on left1.label = left2.label
- and left1.id < left2.id
-where left1.l_f_add::int between left2.l_f_add::int and left2.l_t_add::int
- or left1.l_t_add::int between left2.l_f_add::int and left2.l_t_add::int;
+ and left1.oirid < left2.oirid
+where left1.r_f_add::int between left1.r_f_add::int and left1.r_t_add::int
+ or left2.r_t_add::int between left2.r_f_add::int and left2.r_t_add::int;
