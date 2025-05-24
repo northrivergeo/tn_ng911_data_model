e3c1d283e49899e57222bdb09198e836d5188c17
rjhale1971 <rjhale@northrivergeographic.com>
2025-05-24 15:46:49 -0400

    Added the ESRI File GDB export

---
+++ export_scripts/README.md
@@ -0,0 +1,9 @@
+**The exportesrigdbd is the preferred way to export the data** 
+**The state needs an ESRI File Geodatabase so I fill in an "empty one" with the data from PostgreSQL" 
+**Things you need to know:** 
+
+-host which would be an ip address 
+-user which would be a database user 
+-password which would be the above users password 
+-location of the Template and the TCSTransport Upload Folder
+
--- export_scripts/exportesrigdb.bat
+++ export_scripts/exportesrigdb.bat
@@ -1,6 +1,16 @@
-IF EXIST C:\OSGeo4W64\bin SET PATH=%PATH%;C:\OSGeo4W64\bin
+IF  EXIST C:\OSGeo4W\bin SET PATH=%PATH%;C:\OSGeo4W\bin
 
-ogr2ogr -f OpenFileGDB  TCStransport.gdb -nlt point  PG:"host=host user=user dbname=dbname password=pass" "tn911.address_points" -nln address_points 
-ogr2ogr -f OpenFileGDB  TCStransport.gdb -append -nlt linestring  PG:"host=host user=user dbname=dbname password=pass" "tn911.centerlines" -nln centerlines 
-ogr2ogr -f OpenFileGDB  TCStransport.gdb -append -nlt polygon  PG:"host=host user=user dbname=dbname password=pass" "tn911.esn_boundary" -nln esn_boundary
+ogr2ogr -f OpenFileGDB c:\path\to\export\Template_TCSTransport.gdb c:\path\to\upload\TCSTransport.gdb
+
+ogr2ogr -f OpenFileGDB -append C:\TCSTransport\Upload\TCSTransport.gdb -nln "Addresses" PG:"host=host user=user dbname=dbname password=password" "tn911.addresses" -nln "addresses"
+
+ogr2ogr -f OpenFileGDB -append C:\TCSTransport\Upload\TCSTransport.gdb -nln "Centerlines" PG:"host=host user=user dbname=dbname password=password" "tn911.centerlines" -nln "centerlines"
+
+ogr2ogr -f OpenFileGDB -append C:\TCSTransport\Upload\TCSTransport.gdb -nln "ESN" PG:"host=host user=user dbname=dbname password=password" "tn911.esn_boundary" -nln "esn_boundary"
+
+ogr2ogr -f OpenFileGDB -append C:\TCSTransport\Upload\TCSTransport.gdb PG:"host=host user=user dbname=dbname password=password"  "tn911.esb_law" -nln "esb_law"
+
+ogr2ogr -f OpenFileGDB -append C:\TCSTransport\Upload\TCSTransport.gdb PG:"host=host user=user dbname=dbname password=password"  "tn911.esb_fire" -nln "esb_fire"
+
+ogr2ogr -f OpenFileGDB -append C:\TCSTransport\Upload\TCSTransport.gdb PG:"host=host user=user dbname=dbname password=password"  "tn911.esb_ems" -nln "esb_ems"
 
